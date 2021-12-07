4c4fa6eb966f8ad306cc87de214e4b5a46535f49
Peter Marschall <PDMarschall@hotmail.com>
2021-12-07 10:30:27 +0100

    base class structure

--- OO3.LottoGenerator/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OO3.Lottery_Functionality
-{
-    public class Class1
-    {
-    }
-}
---
+++ OO3.LottoGenerator/Joker/JokerNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO3.Lottery_Functionality.Joker
+{
+    class JokerNumber
+    {
+    }
+}
