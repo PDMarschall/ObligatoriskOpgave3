83759b31a34faa3cb0734f4e36bb2bd643425e77
Peter Marschall <PDMarschall@hotmail.com>
2021-12-08 14:20:04 +0100

    test grid

--- ObligatoriskOpgave3/MainWindow.xaml.cs
+++ ObligatoriskOpgave3/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OO3.Lottery_Functionality.Coupon.Standard;
 
 namespace OO3.WPF_UI
 {
