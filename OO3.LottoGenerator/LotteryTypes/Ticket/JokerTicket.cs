23839a3be9c6fdc6275f4647793ea54d74bbf3d5
Peter Marschall <PDMarschall@hotmail.com>
2021-12-09 12:48:50 +0100

    abstract coupon

---
+++ OO3.LottoGenerator/LotteryTypes/Ticket/JokerTicket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OO3.Lottery_Functionality.Abstract_Classes;
+
+namespace OO3.Lottery_Functionality.LotteryTypes.Ticket
+{
+    public class JokerTicket
+    {
+
+    }
+}
--- OO3.LottoGenerator/LotteryTypes/Ticket/StandardTicket.cs
+++ OO3.LottoGenerator/LotteryTypes/Ticket/StandardTicket.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OO3.Lottery_Functionality.Abstract_Classes;
 
-namespace OO3.Lottery_Functionality.Abstract_Classes
+namespace OO3.Lottery_Functionality.LotteryTypes.Ticket
 {
-    class Ticket : ICoupon
+    class StandardTicket
     {
 
     }
