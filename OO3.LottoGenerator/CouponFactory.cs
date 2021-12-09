e5c1389ad1ba1217b00b3e79cc97dd3b7458bf34
Peter Marschall <PDMarschall@hotmail.com>
2021-12-09 13:30:20 +0100

    factory up and running

--- OO3.LottoGenerator/CouponFactory.cs
+++ OO3.LottoGenerator/CouponFactory.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OO3.Lottery_Functionality.LotteryTypes.Ticket;
 
 namespace OO3.Lottery_Functionality
 {
-    class CouponFactory
+    public static class CouponFactory
     {
+        public static ICoupon GetCoupon(string couponType)
+        {
+            ICoupon ticket = null;
 
+            switch (couponType)
+            {
+                case "Standard":
+                    ticket = new StandardTicket();
+                    break;
+                case "Joker":
+                    ticket = new JokerTicket();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid repository type");
+            }
+
+            return ticket;
+        }
     }
 }
--- OO3.LottoGenerator/ICoupon.cs
+++ OO3.LottoGenerator/ICoupon.cs
@@ -7,7 +7,7 @@ using OO3.Lottery_Functionality.Abstract_Classes;
 
 namespace OO3.Lottery_Functionality
 {
-    interface ICoupon
+    public interface ICoupon
     {
         bool VerifyRow(IList<Row> previousRows, Row currentRow, int iteration);
     }
