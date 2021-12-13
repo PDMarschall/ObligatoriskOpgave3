03a4853bffbad2a90b4f12e30b33f18a785204b4
Peter Marschall <PDMarschall@hotmail.com>
2021-12-13 09:48:14 +0100

    timestamp format

--- OO3.IO_Functionality/FileSaver.cs
+++ OO3.IO_Functionality/FileSaver.cs
@@ -10,11 +10,11 @@ namespace OO3.IO_Functionality
         const string path = @"D:\Temp\";
         public static void SaveCouponTxt(IList<ICoupon> coupons)
         {
-            Directory.CreateDirectory(path + $"{DateTime.Today.ToString("dd-M-yyyy-t")}");
+            Directory.CreateDirectory(path + $"{DateTime.Today.ToString("dd-M-yyyy")}");
 
             for (int i = 0; i < coupons.Count; i++)
             {
-                StreamWriter writer = new StreamWriter(path + $"{DateTime.Today.ToString("dd-M-yyyy-t")}\\LotteryCoupon{i + 1:0#}.txt");
+                StreamWriter writer = new StreamWriter(path + $"{DateTime.Today.ToString("dd-M-yyyy")}\\LotteryCoupon{i + 1:0#}.txt");
 
                 writer.WriteLine(coupons[i]);
                 writer.Close();
